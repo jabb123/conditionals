934e43813d2eea5a618a41d28a082663abae83fc
jabb123 <jabbinante@sjnma.org>
2017-02-23 10:32:32 -0600

    [Problem 3 complete]

--- Problem3.py
+++ Problem3.py
@@ -1,7 +1,17 @@
 print("Welcome to Problem3, please enter a number and how you are feeling today")
 
 number = int(raw_input("number"))
-mood = int(raw_input("mood"))
+mood = str(raw_input("mood"))
 
 if(number>=5):
 	print("in range")
+if(number<=25):
+	print("in range")
+if(mood =="good"):
+	print(":-o")
+if(mood =="Good"):
+	print(":-o")
+if(number >5):
+	print(":-o")
+if(number <25):
+	print(":-o")
